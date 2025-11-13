8cbfa59e91a6dd0d8666cd7da8f347ec6216decb
Colin <colinleonghongseng@gmail.com>
2025-11-13 18:22:46 +0800

    added update and delete endpoints for course

--- app/routers/course.py
+++ app/routers/course.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.schemas.course import CourseCreate, CourseGet
+from app.schemas.course import CourseCreate, CourseGet, CourseUpdate
 from app.services import course_service
 from app.dependencies import get_db
 
@@ -21,4 +21,11 @@ def get_course(course_id: int, db: Session = Depends(get_db)):
     return course_service.get_course(db=db, course_id=course_id)
 
 # Update
+@router.patch('/{course_id}', response_model=CourseGet)
+def update_course(course_id: int, course: CourseUpdate, db: Session = Depends(get_db)):
+    return course_service.update_course(db=db, course_id=course_id, course=course)
 
+# Delete
+@router.delete('/{course_id}')
+def delete_course(course_id: int, db: Session = Depends(get_db)):
+    course_service.
