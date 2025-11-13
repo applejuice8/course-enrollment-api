0943c2aa9a6f7dc6058fb5add25d52d87cc68b84
Colin <colinleonghongseng@gmail.com>
2025-11-13 19:16:03 +0800

    changed composite key of enrollment to be declared directly

--- app/models/enrollment.py
+++ app/models/enrollment.py
@@ -6,16 +6,12 @@ from datetime import datetime
 class Enrollment(Base):
     __tablename__ = 'enrollment'
 
-    student_id: Mapped[int] = mapped_column(ForeignKey('student.id'))
-    course_id: Mapped[int] = mapped_column(ForeignKey('course.id'))
+    student_id: Mapped[int] = mapped_column(ForeignKey('student.id'), primary_key=True)
+    course_id: Mapped[int] = mapped_column(ForeignKey('course.id'), primary_key=True)
     date: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
     student: Mapped[int] = relationship('Student', back_populates='enrollments')
     course: Mapped[int] = relationship('Course', back_populates='enrollments')
-
-    __table_args__ = (
-        PrimaryKeyConstraint('student_id', 'course_id', name='pk_enrollment'),     # Composite key
-    )
 
     def __repr__(self):
         return f'<Enrollment(student_id={self.student_id}, course_id={self.course_id}, date={self.date})>'
